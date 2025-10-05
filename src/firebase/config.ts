03c18631a8fc4ab4bcc88ab12007d1d232c9b487
NUB CODER <nubcoders@gmail.com>
2025-10-05 09:23:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2732907414-d12f4",
+  "appId": "1:685979242181:web:fb2a1f60e946c43fe4eaac",
+  "apiKey": "AIzaSyBdLk8midWQuvwo7sWORDDO41mTzoYMAXE",
+  "authDomain": "studio-2732907414-d12f4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "685979242181"
+};
